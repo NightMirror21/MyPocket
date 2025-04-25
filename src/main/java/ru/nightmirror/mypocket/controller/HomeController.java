db6b2e6c1a7259ddc086ffc18ef8be4c06036e15
NightMirror <me@nightmirror.ru>
2025-04-26 00:11:31 +0300

    Refactor controllers to use @RequestMapping at class level

--- src/main/java/ru/nightmirror/mypocket/controller/HomeController.java
+++ src/main/java/ru/nightmirror/mypocket/controller/HomeController.java
@@ -2,11 +2,13 @@ package ru.nightmirror.mypocket.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping()
     public String home() {
         return "home";
     }
--- src/main/java/ru/nightmirror/mypocket/controller/WelcomeController.java
+++ src/main/java/ru/nightmirror/mypocket/controller/WelcomeController.java
@@ -2,11 +2,13 @@ package ru.nightmirror.mypocket.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class WelcomeController {
 
-    @GetMapping("/")
+    @GetMapping()
     public String welcome() {
         return "welcome";
     }

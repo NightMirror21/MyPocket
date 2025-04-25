0008cccce06fd983e1c00d06274b23b185e1b09c
NightMirror <me@nightmirror.ru>
2025-04-25 19:26:00 +0300

    Initialize project with Spring Boot setup

---
+++ src/main/java/ru/nightmirror/mypocket/MyPocketApplication.java
@@ -0,0 +1,13 @@
+package ru.nightmirror.mypocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nightmirror/mypocket/MyPocketApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nightmirror.mypocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
